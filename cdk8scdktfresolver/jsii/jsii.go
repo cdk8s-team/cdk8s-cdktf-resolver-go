ad2c99f6a7830d8327eb1773a39db31765cbf684
cdk8s-automation <cdk8s-team@amazon.com>
2025-12-12 12:21:18 +0000

    chore(release): v0.0.217

--- cdk8scdktfresolver/jsii/jsii.go
+++ cdk8scdktfresolver/jsii/jsii.go
@@ -14,7 +14,7 @@ import (
 	cdktf      "github.com/hashicorp/terraform-cdk-go/cdktf/jsii"
 )
 
-//go:embed cdk8s-cdktf-resolver-0.0.216.tgz
+//go:embed cdk8s-cdktf-resolver-0.0.217.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -26,5 +26,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@cdk8s/cdktf-resolver", "0.0.216", tarball)
+	_jsii_.Load("@cdk8s/cdktf-resolver", "0.0.217", tarball)
 }
